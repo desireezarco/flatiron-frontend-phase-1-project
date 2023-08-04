022e4396645d08f2f4ac329d7d683f85ef2f0e86
Desiree Zarco <desireeannzarco7@gmail.com>
2023-08-04 07:30:30 -0700

    getting elements loaded on DOM

--- index.js
+++ index.js
@@ -1,6 +1,156 @@
 /*  UNFINISHED REVISION OF PROJECT!!! */
 
 
+function fetchMembers() {
+    return fetch("http://localhost:3000/members")
+        .then((res) => res.json())
+        .then((json) => renderMembers(json));
+}
+
+function renderMembers(members) {
+    
+    const section = document.querySelector('section');
+    members.forEach((member) => {
+        const h2 = document.createElement('h2');
+        h2.innerHTML = member.name;
+        section.appendChild(h2);
+    })
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetchMembers();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", async() => {
+//     let data = [];
+// console.log(data)
+//     try {
+//             data = await loadData(); 
+//     } catch (e) {
+//         // console.log("ERROR!");
+//         // console.log(e);
+//     }
+//     // console.log(loadData);
+// })
+
+//Create a card for each member
+
+// function loadData(members){
+//     let member = document.createElement('div');
+//     // console.log(card)
+//     member.classList.add('card')
+    
+//     let img = document.createElement('img')
+    
+//     img.classList.add('member-image')
+//     // img.src = data.image;
+    
+    
+//     // card.append(div, img)
+//     document.body.append("game-container")
+//     // console.log(member)
+// }
+
+
+
+
+
+
+// const API = "http://localhost:3000/members";
+// const display = document.querySelector("#display-members");
+
+// //grab json data using fetch
+//async json fetch
+
+
+// const members = fetch("http://localhost:3000/members")
+// .then((response) => {
+//     console.log('SUCCESS', response);
+//     return response.json();
+// })
+// .then(data => {
+//     // renderCards(data)
+// }) 
+// .catch((error) => {
+//     console.log('UNSUCCESSFUL', error);
+// });
+
+
+// const getData = async() => {
+//     const response = await fetch(API);
+//     const data = await response.json();
+//     return data
+// }
+
+// const displayMembers = async() => {
+//     const renderCards = await getData();
+
+// }
+// displayMembers();  
+
+  
+    // const image = document.getElementById("display-members");
+    // console.log(cards)
+    
+    // console.log(cards)
+    // image.src = images
+    // document.body.appendChild(cards)
+    
+    
+    // renderCards.forEach((card) => {
+        // console.log(object)
+        // const member = document.getElementById('display-members')
+        // console.log(renderCards)
+
+        // card.appendChild
+        // const member = document.createElement('div');
+        // member.setAttribute ('src', './images/')
+        // document.body.appendChild(image)
+    // console.log(member)
+
+        
+    // });
+
+
+
+
+
+
+
+
+
+//******* have each card appear **********
+// function renderCards(members){
+    // const cards = members.map((member) => {
+    //     // const card = document.createElement("div");
+    //     // const front = document.createElement("img");
+    //     // const back = document.createElement("div");
+    //     const front = document.createElement("div")
+    //     front.textContent = member.name
+    //     return members
+
+// }
+
+// return cards and render
+
+// const cards = document.querySelectorAll('game-container');
+
 // const API = "http://localhost:3000/members"
 
 // fetch(API)
@@ -20,70 +170,6 @@
 
 
 
-
-
-// //grab json data using fetch
-
-// fetch ("http://localhost:3000/members")
-//     .then((resp) => resp.json())
-//     .then((data) => renderCards(data))
-//     .catch(error => console.log("ERROR"))
-//     .catch(error => {
-//         console.log(error);
-//     })
-
-
-//async json fetch
-// method: 'POST',
-// headers: {
-    //     'Content-Type': 'application/json'
-    // },
-    // body: JSON.stringify({'id':1})
-    // })
-    
-    
-const members = fetch("http://localhost:3000/members")
-    .then((response) => {
-        console.log('SUCCESS', response);
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-        renderCards(data)
-    }) 
-    .catch((error) => {
-        console.log('UNSUCCESSFUL', error);
-    });
-console.log(members)
-  
-
-
-
-// const response = await fetch("http://localhost:3000/members");
-// const json = await response.json();
-// console.log(JSON.stringify(json))
-
-
-
-//******* have each card appear (???)**********
-function renderCards(members){
-    const cards = members.map((member) => {
-        // const card = document.createElement("div");
-        // const front = document.createElement("img");
-        // const back = document.createElement("div");
-        const front = document.createElement("h2")
-        front.textContent = member.name
-        return front
-    })
-    console.log(cards)
-}
-
-// return cards and render
-
-
-
-
-
 // create 20 divs, two each for matching pair(???)
 //create grid(???)
 
@@ -92,8 +178,6 @@ function renderCards(members){
 //   })
 //         card.classList = "card";
 //         front.classList = document.createElement("img");
-//         // img.classList.add("member-image");
-//         // img.src = member.image;
 //         back.classList = "back";
 
 
@@ -110,78 +194,77 @@ function renderCards(members){
 // const cards = document.querySelector('card-container')
 // const cards = document.getElementById('members')
 
-const cards = document.querySelectorAll('.memory-card');
 
 
 
 
-let flippedCard = false;
-let lockBoard= false;
-let firstCard, secondCard;
+// let flippedCard = false;
+// let lockBoard= false;
+// let firstCard, secondCard;
 
 
-//flip card action
-function flipCard(){
-    if (lockBoard) return;
-    if (this === firstCard) return;
+// //flip card action
+// function flipCard(){
+//     if (lockBoard) return;
+//     if (this === firstCard) return;
 
-    this.classList.add('flip');
+//     this.classList.add('flip');
 
-    if (!flippedCard){
-        //first click
-        flippedCard = true;
-        firstCard = this;
+//     if (!flippedCard){
+//         //first click
+//         flippedCard = true;
+//         firstCard = this;
     
-        return;
-    }
-        //second click
-        secondCard = this;
+//         return;
+//     }
+//         //second click
+//         secondCard = this;
         
-        checkForMatch();
-    }
+//         checkForMatch();
+//     }
 
-//Check for match
-function checkForMatch(){
-    let isMatch = firstCard.dataset.member === secondCard.dataset.member;
+// //Check for match
+// function checkForMatch(){
+//     let isMatch = firstCard.dataset.member === secondCard.dataset.member;
     
-    isMatch ? disableCards() : unflipCards();
+//     isMatch ? disableCards() : unflipCards();
 
-//If does not match
-function disableCards(){
-        firstCard.removeEventListener("click", flipCard);
-        secondCard.removeEventListener("click", flipCard);
+// //If does not match
+// function disableCards(){
+//         firstCard.removeEventListener("click", flipCard);
+//         secondCard.removeEventListener("click", flipCard);
 
-    resetBoard();
-}
+//     resetBoard();
+// }
 
-//Time it takes to flip card back over
-function unflipCards(){
-    lockBoard = true;
-        setTimeout(() => {
-            firstCard.classList.remove("flip");
-            secondCard.classList.remove("flip");
+// //Time it takes to flip card back over
+// function unflipCards(){
+//     lockBoard = true;
+//         setTimeout(() => {
+//             firstCard.classList.remove("flip");
+//             secondCard.classList.remove("flip");
             
-        resetBoard();
-        }, 1500);
-    }
-}
+//         resetBoard();
+//         }, 1500);
+//     }
+// }
 
-function resetBoard(){
-    flippedCard = false;
-    lockBoard = false;
-    firstCard = null;
-    secondCard = null;
-}
+// function resetBoard(){
+//     flippedCard = false;
+//     lockBoard = false;
+//     firstCard = null;
+//     secondCard = null;
+// }
 
-//Shuffle cards to different positions
-function shuffle(){
-    cards.forEach(card => {
-        let randomPosition = Math.random() *  12;
-        card.style.order = randomPosition ;
+// //Shuffle cards to different positions
+// function shuffle(){
+//     cards.forEach(card => {
+//         let randomPosition = Math.random() *  12;
+//         card.style.order = randomPosition ;
       
-    });
-}
-shuffle();
+//     });
+// }
+// shuffle();
 
 
 //FINISH ADDING Event listeners ***
