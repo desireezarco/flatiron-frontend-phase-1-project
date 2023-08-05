9e2259a98d210e767978734c5970bb4c8c2741be
Desiree Zarco <desireeannzarco7@gmail.com>
2023-08-05 12:27:56 -0700

    rewrote css js html

--- index.js
+++ index.js
@@ -1,29 +1,110 @@
 /*  UNFINISHED REVISION OF PROJECT!!! */
 
+const gameContainer = document.querySelector(".game-container");
+// const memoryCard = document.querySelector(".memory-card");
 
-function fetchMembers() {
-    return fetch("http://localhost:3000/members")
-        .then((res) => res.json())
-        .then((json) => renderMembers(json));
-}
+let members = [];
+let firstCard, secondCard;
+let lockBoard = false;
+let score = 0;
 
-function renderMembers(members) {
-    
-    const section = document.querySelector('section');
-    members.forEach((member) => {
-        // const h2 = document.createElement('h2');
-        // h2.innerHTML = member.name;
-        // section.appendChild(h2);
+document.querySelector(".score").textContent = score;
 
-        const memberImage = document.createElement('img');
-        memberImage.src = member.image;
-        section.append(memberImage);
+// url = "http://localhost:3000/members"
+
+//Fetch data
+fetch("http://localhost:3000/members")
+    .then((res) => res.json())
+    .then((data) => {
+        cards = [...data]
+        console.log(cards)
+        shuffleCards()
+        generateCards()
     })
+
+
+function shuffleCards() {
+    let currentIndex = cards.length, 
+    randomIndex,
+    temporaryValue;
+
+    while (currentIndex !==0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = cards[currentIndex];
+        cards[currentIndex] = cards[randomIndex];
+        cards[randomIndex] = temporaryValue;
+    }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    fetchMembers();
-})
+function generateCards() {
+    for (let card of cards) {
+        const cardElement = document.createElement('div')
+        cardElement.classList.add('card')
+        cardElement.setAttribute('name', card.name)
+        cardElement.setAttribute('src', card.image)
+        cardElement.innerHTML = `
+        <div class="front">
+            <img class="front-face" src=${card.image} />
+            </div>
+        <div class="back-face"></div>
+            `
+        gameContainer.appendChild(cardElement)
+        // gameContainer.appendChild(cardElement)
+        cardElement.addEventListener("click", flipCard)
+}
+}
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add("flipped")
+
+    if(!firstCard){
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    score++;
+    document.querySelector(".score").textContent = score;
+    lockBoard = true;
+
+    checkIfMatch();
+}
+
+function checkIfMatch(){
+    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListener("clicl", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+
+    resetBoard();
+}
+
+function unflipCards(){
+    setTimeout(() => {
+        firstCard.classList.remove("flipped")
+        secondCard.classList.remove("flipped");
+        resetBoard();
+    }, 1000);
+}
+
+function resetBoard(){
+    firstCard = null;
+    secondCard = null;
+    lockBoard = false;
+}
+
+function restart(){
+    resetBoard();
+    shuffleCards
+}
 
 
 
@@ -41,238 +122,119 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-// document.addEventListener("DOMContentLoaded", async() => {
-//     let data = [];
-// console.log(data)
-//     try {
-//             data = await loadData(); 
-//     } catch (e) {
-//         // console.log("ERROR!");
-//         // console.log(e);
-//     }
-//     // console.log(loadData);
-// })
 
-//Create a card for each member
 
-// function loadData(members){
-//     let member = document.createElement('div');
-//     // console.log(card)
-//     member.classList.add('card')
+
+
+
+
+
+
+
+
+
+
     
-//     let img = document.createElement('img')
-    
-//     img.classList.add('member-image')
-//     // img.src = data.image;
-    
-    
-//     // card.append(div, img)
-//     document.body.append("game-container")
-//     // console.log(member)
-// }
 
 
 
-
-
-
-// const API = "http://localhost:3000/members";
-// const display = document.querySelector("#display-members");
-
-// //grab json data using fetch
-//async json fetch
-
-
-// const members = fetch("http://localhost:3000/members")
-// .then((response) => {
-//     console.log('SUCCESS', response);
-//     return response.json();
-// })
-// .then(data => {
-//     // renderCards(data)
-// }) 
-// .catch((error) => {
-//     console.log('UNSUCCESSFUL', error);
-// });
-
-
-// const getData = async() => {
-//     const response = await fetch(API);
-//     const data = await response.json();
-//     return data
-// }
-
-// const displayMembers = async() => {
-//     const renderCards = await getData();
-
-// }
-// displayMembers();  
-
-  
-    // const image = document.getElementById("display-members");
-    // console.log(cards)
-    
-    // console.log(cards)
-    // image.src = images
-    // document.body.appendChild(cards)
-    
-    
-    // renderCards.forEach((card) => {
-        // console.log(object)
-        // const member = document.getElementById('display-members')
-        // console.log(renderCards)
-
-        // card.appendChild
-        // const member = document.createElement('div');
-        // member.setAttribute ('src', './images/')
-        // document.body.appendChild(image)
-    // console.log(member)
-
-        
-    // });
-
-
-
-
-
-
-
-
-
-//******* have each card appear **********
-// function renderCards(members){
-    // const cards = members.map((member) => {
-    //     // const card = document.createElement("div");
-    //     // const front = document.createElement("img");
-    //     // const back = document.createElement("div");
-    //     const front = document.createElement("div")
-    //     front.textContent = member.name
-    //     return members
-
-// }
-
-// return cards and render
-
-// const cards = document.querySelectorAll('game-container');
-
-// const API = "http://localhost:3000/members"
-
-// fetch(API)
-//     .then((res) => res.json())
-//     .then(renderMembers);
-
-// function renderMembers(members){
-//     members.forEach(renderMembers);
-// }
-
-// function renderMembers(member){
-//     const memberDiv = ('member-card')
-//     const memberImage = document.createElement("img");
-//     memberImage.src = member.image;
-//     memberDiv.append(memberImage)
-// }
-
-
-
-// create 20 divs, two each for matching pair(???)
-//create grid(???)
-
-
-
-//   })
-//         card.classList = "card";
-//         front.classList = document.createElement("img");
-//         back.classList = "back";
-
-
-        // card.append(members)
-
-        // document.querySelector("#card-container")
-
-// })
-// }
-///practice code -- (not sure if i'm going the right direction somewhere here)
-
-// const cards = document.getElementById("")
-// console.log(cards)
-// const cards = document.querySelector('card-container')
-// const cards = document.getElementById('members')
-
-
-
-
-
-// let flippedCard = false;
-// let lockBoard= false;
-// let firstCard, secondCard;
-
-
-// //flip card action
 // function flipCard(){
 //     if (lockBoard) return;
 //     if (this === firstCard) return;
 
-//     this.classList.add('flip');
+//     this.classList.add("flipped");
 
-//     if (!flippedCard){
-//         //first click
-//         flippedCard = true;
+//     if (!firstCard){
 //         firstCard = this;
-    
 //         return;
 //     }
-//         //second click
-//         secondCard = this;
-        
-//         checkForMatch();
-//     }
 
-// //Check for match
-// function checkForMatch(){
-//     let isMatch = firstCard.dataset.member === secondCard.dataset.member;
-    
-//     isMatch ? disableCards() : unflipCards();
+// secondCard = this;
+// score++;
+// document.querySelector(".score").textContent = score;
+// lockBoard = true;
 
-// //If does not match
-// function disableCards(){
-//         firstCard.removeEventListener("click", flipCard);
-//         secondCard.removeEventListener("click", flipCard);
+// checkIfMatch()
 
-//     resetBoard();
 // }
 
-// //Time it takes to flip card back over
+// function checkIfMatch(){
+//     let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+
+//     isMatch ? disableCards() : unflipCards();
+// }
+
+// function disableCards() {
+//     firstCard.removeEventListener("click", flipCard)
+//     secondCard.removeEventListener("click", flipCard)
+
+//     resetBoard()
+// }
+
 // function unflipCards(){
-//     lockBoard = true;
-//         setTimeout(() => {
-//             firstCard.classList.remove("flip");
-//             secondCard.classList.remove("flip");
-            
-//         resetBoard();
-//         }, 1500);
-//     }
+//     setTimeout(() => {
+//         firstCard.classList.remove("flipped")
+//         secondCard.classList.remove("flipped")
+//         resetBoard()
+//     }, 1000)
 // }
 
 // function resetBoard(){
-//     flippedCard = false;
-//     lockBoard = false;
-//     firstCard = null;
-//     secondCard = null;
+//     firstCard = null
+//     secondCard = null
+//     lockBoard = false
 // }
 
-// //Shuffle cards to different positions
-// function shuffle(){
-//     cards.forEach(card => {
-//         let randomPosition = Math.random() *  12;
-//         card.style.order = randomPosition ;
-      
-//     });
+// function restart(){
+//     resetBoard()
+//     shuffleCards()
+//     score = 0;
+//     document.querySelector(".score").textContent = score
+//     gameContainer.innerHTML = "",
+//     generateCards()
 // }
-// shuffle();
 
 
-//FINISH ADDING Event listeners ***
-// cards.forEach(card => card.addEventListener("click", flipCard)); (???)
 
-//[ ] NEED ONE MORE EVENT LISTENER (Not another click event)
-//[ ] EXTRA EVENT LISTENER
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // ******* have each card appear **********
+// function renderMembers(members) {
+
+//     const div = document.querySelector('div');
+//     members.forEach((member) => {
+//         // const h2 = document.createElement('h2');
+//         // h2.innerHTML = member.name;
+//         // section.appendChild(h2);
+
+//         // const div = document.createElement('div');
+//         // section.append(div)
+ 
+//         const memberImage = document.createElement('img');
+//         memberImage.src = member.image;
+//         div.append(memberImage);
+        
+//         // card.classList = "card";
+//         // front.classList = document.createElement("img");
+//         // back.classList = "back";
+
+//         // card.addEventListener('click', flipCard)
+//         // gameContainer.appendChild(card)
+//     })
+// }
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     fetchMembers();
+// })
+
